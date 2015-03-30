fe6c19de11750672ffadbbda95a9f52557271e0c
arctarus44 <arthur.dewarumez@gmail.com>
2015-03-30 10:33:19 +0200

    Test.

--- ase_kmod.h
+++ ase_kmod.h
@@ -5,10 +5,13 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
+#include <linux/init.h>
+#include <linux/seq_file.h>
 
 #define MOD_NAME "ASE_KMOD :"
 
 #define PROC_DIR "ase"
+#define PROC_ENTRY "ase_cmd"
 
 
 #define LOG_INIT "Initialisation... Done!\n"
