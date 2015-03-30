b552ee350c0b7426c1bbe23f5227fdfff5ee342a
arctarus44 <arthur.dewarumez@gmail.com>
2015-03-30 10:29:50 +0200

    Test.

--- ase_kmod.c
+++ ase_kmod.c
@@ -5,15 +5,41 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A kernel module which monitors the execution time of a task on the different cores of a processor.");
 
 static struct proc_dir_entry *proc_folder;
+int test(struct file *file, const char __user *x, size_t size, loff_t *data){
+	/* get buffer size */
+	procfs_buffer_size = count;
+	if (procfs_buffer_size > PROCFS_MAX_SIZE ) {
+		procfs_buffer_size = PROCFS_MAX_SIZE;
+	}
+
+	/* write data to the buffer */
+	if ( copy_from_user(procfs_buffer, buffer, procfs_buffer_size) ) {
+		return -EFAULT;
+	}
+
+	return procfs_buffer_size;
+}
+
+static const struct file_operations ase_fops = {
+	.owner = THIS_MODULE,
+	.open = NULL,
+	/* .read = seq_read, */
+	/* .llseek = seq_lseek, */
+	.release = single_release,
+	.write = test,
+};
+
 
 int ase_kmod_init(void){
 	proc_folder = proc_mkdir(PROC_DIR, NULL);
 	printk(KERN_EMERG MOD_NAME LOG_INIT);
+	proc_create(PROC_ENTRY, 0, NULL, &ase_fops);
 	return 0;
 }
 
 void ase_kmod_cleanup(void){
 	remove_proc_entry(PROC_DIR, NULL);
+	remove_proc_entry(PROC_ENTRY, NULL);
 	printk(KERN_EMERG MOD_NAME LOG_CLEAN);
 }
 
