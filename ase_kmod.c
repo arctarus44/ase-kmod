23f6399ec615a95f4f1d018b428e5facd76ad372
Jean-Serge <j.monbailly@orange.Fr>
2015-03-30 11:33:54 +0200

    Définition de fonctions pour la structure file_operations

--- ase_kmod.c
+++ ase_kmod.c
@@ -5,26 +5,41 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A kernel module which monitors the execution time of a task on the different cores of a processor.");
 
 static struct proc_dir_entry *proc_folder;
-int test(struct file *file, const char *data, size_t size, loff_t *offset){
-  printk(KERN_EMERG MOD_NAME "Passage dans la fonction de write.\n");    
-
+int my_write_function(struct file *file, const char *data, size_t size, loff_t *offset){
+  printk(KERN_EMERG MOD_NAME " passage dans la fonction write.\n");    
+  printk(KERN_EMERG MOD_NAME " data : %s\n", data);    
+  printk(KERN_EMERG MOD_NAME " size : %d\n", size);    
   return size;
+}
+
+static ssize_t my_read_function(struct file *file, char *buf, size_t count, loff_t *ppos)
+{
+  printk(KERN_EMERG MOD_NAME " passage dans la fonction read.\n");    
+  printk(KERN_EMERG MOD_NAME " donées lues : %s\n", buf);    
+  return count;
+}
+
+static int my_open_function(struct inode *inode, struct file *file)
+{
+  printk(KERN_EMERG MOD_NAME " passage dans la fonction open.\n");    
+
+    return 0;
 }
 
 static const struct file_operations ase_fops = {
 	.owner = THIS_MODULE,
-	/* .open = NULL, */
-	/* .read = seq_read, */
+	.open = my_open_function,
+	.read = my_read_function,
 	/* .llseek = seq_lseek, */
 	/* .release = single_release, */
-	.write = test,
+	.write = my_write_function,
 };
 
 
 int ase_kmod_init(void){
 	proc_folder = proc_mkdir(PROC_DIR, NULL);
 	printk(KERN_EMERG MOD_NAME LOG_INIT);
-	proc_create(PROC_ENTRY, 0, NULL, &ase_fops);
+	proc_create(PROC_ENTRY, 0644, NULL, &ase_fops);
 	return 0;
 }
 
