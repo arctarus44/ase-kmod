6d2feafb1d77cc0d816fc892a3e7239ae8ea96af
arctarus44 <arthur.dewarumez@gmail.com>
2015-03-31 11:40:45 +0200

    Début implémentation de la partie 2.

--- ase_kmod.c
+++ ase_kmod.c
@@ -27,8 +27,8 @@ static const struct file_operations ase_fops = {
 };
 
 static struct proc_dir_entry *proc_folder;
-
-
+static int pid_count = 0;
+static struct pid *pid_array[MAX_PID_HANDLE];
 
 static int ase_cmd_show(struct seq_file *m, void *v){
 	seq_printf(m, "lolilol\n");
@@ -39,10 +39,43 @@ static int ase_cmd_open(struct inode *inode, struct file *file){
 	return single_open(file, ase_cmd_show, NULL);
 }
 
-static int init_track_pid(struct file *file, const char *data, size_t size, loff_t *offset){
-	printk(KERN_EMERG MOD_NAME " passage dans la fonction write.\n");
-	printk(KERN_EMERG MOD_NAME " data : %s\n", data);
-	printk(KERN_EMERG MOD_NAME " size : %d\n", size);
+static void add_pid_action(const char *pid_str){
+	long pid;
+	struct pid *pid_struct;
+	switch(kstrtol(pid_str, 10, &pid)){
+	case -ERANGE:
+		printk(KERN_EMERG MOD_NAME ERR_INIT_OVERFLOW);
+		return ;
+	case -EINVAL:
+		printk(KERN_EMERG MOD_NAME ERR_INIT_NOT_INT);
+		return ;
+	}
+	if((pid_struct = find_get_pid(pid)) != NULL){
+		printk(KERN_EMERG MOD_NAME LOG_ADD_PID);
+		proc_create(pid_str, 0644, proc_folder, &ase_fops);
+		pid_array[pid_count] = pid_struct;
+		pid_count++;
+	}
+	else{
+		printk(KERN_EMERG MOD_NAME ERR_INIT_NOT_PID);
+	}
+}
+
+static int init_track_pid(struct file *file, const char __user *buff, size_t size, loff_t *data){
+	char *tmp = kmalloc(sizeof(char) * size, 0);
+	int i;
+	printk(KERN_EMERG MOD_NAME LOG_INIT_TRACK);
+	if (size > (MOD_BUF_LEN - 1)) {
+		printk(KERN_EMERG MOD_NAME "Message trop grand.\n");
+		return -EINVAL;
+	}
+	for(i = 0; i < size-1; i++){
+		tmp[i] = buff[i];
+	}
+	tmp[i] = '\0';
+
+	add_pid_action(tmp);
+
 	return size;
 }
 
--- ase_kmod.h
+++ ase_kmod.h
@@ -7,14 +7,27 @@
 #include <linux/proc_fs.h>
 #include <linux/init.h>
 #include <linux/seq_file.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
 
 #define MOD_NAME "ASE_KMOD :"
+#define MOD_BUF_LEN 64
 
 #define PROC_DIR "ase"
 #define PROC_ENTRY "ase_cmd"
+#define MAX_PID_HANDLE 1024
 
 
-#define LOG_INIT "Initialisation... Done!\n"
-#define LOG_CLEAN "Cleanup module... Done!\n"
+#define LOG_INIT " Initialisation... Done!\n"
+#define LOG_CLEAN " Cleanup module... Done!\n"
+#define LOG_INIT_TRACK " Going into the init_track_pid function.\n"
+#define LOG_ADD_PID " Adding a new PID into the module.\n"
+
+#define ERR_INIT_OVERFLOW " Overflow during conversion.\n"
+#define ERR_INIT_NOT_INT " Error during string to int conversion.\n"
+#define ERR_INIT_NOT_PID " The value does not match any PID.\n"
+
+
 
 #endif
