5c80f60dd27e3b53c2843b3ce8e4445f10d940b4
Jean-Serge <j.monbailly@orange.Fr>
2015-04-20 11:06:07 +0200

    Implémentation de quelques fonctions pour manipuler une liste de pid.

--- pid_list.h
+++ pid_list.h
@@ -1,8 +1,9 @@
 #ifndef H_PID_LIST
 #define H_PID_LIST
 
-/* #include <linux/pid.h> */
-struct pid;
+#include <linux/pid.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
 
 /*
  * Ce fichier permet de manipuler facilement la liste de pid
@@ -10,5 +11,11 @@ struct pid;
  */
 
 void add_pid(struct pid *pid_s);
+
+struct ase_pid * get_struct(int pid);
+
+long get_stime(int pid);
+
+long get_utime(int pid);
 
 #endif
